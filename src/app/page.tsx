e4359589a3de579b73e1359ea9a2e68608f8197d
karthik <karthiknadar1204@gmail.com>
2024-02-25 15:50:37 +0530

    added database connection to supabase using drizzleorm

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,17 @@
 import { Button } from '@/components/ui/button'
 import React from 'react'
 import FormGenerator from './form-generator'
+import Header from "@/components/ui/header";
+import { SessionProvider } from 'next-auth/react';
 
 const page = () => {
   return (
+  <SessionProvider>
     <main className="flex min-h-screen flex-col items-center">
+       <Header />
       <FormGenerator/>
-  </main>
+    </main>
+  </SessionProvider>
   )
 }
 
