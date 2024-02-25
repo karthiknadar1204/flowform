20a710558a2314eae6c6a96c1080b5f533dbe649
karthik <karthiknadar1204@gmail.com>
2024-02-25 14:53:56 +0530

    integrated openai api to generate form

---
+++ src/app/form-generator/index.tsx
@@ -0,0 +1,78 @@
+"use client";
+import React, { useState, useEffect } from 'react'
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+  DialogFooter
+} from "@/components/ui/dialog"
+import { Button } from "@/components/ui/button";
+import { Textarea } from '@/components/ui/textarea';
+import { Plus } from 'lucide-react';
+import { generateForm } from '../actions/generateForm';
+import { useFormState, useFormStatus } from 'react-dom';
+
+type Props = {}
+
+const initialState: {
+    message: string;
+    data?: any;
+  } = {
+    message: ""
+  }
+
+export function SubmitButton() {
+    const { pending } = useFormStatus();
+    return (
+      <Button type="submit" disabled={pending}>
+        {pending ? "Generating..." : "Generate"}
+      </Button>
+    );
+  }
+
+
+const FormGenerator = (props: Props) => {
+    const [state, formAction] = useFormState(generateForm, initialState);
+    const [open, setOpen] = useState(false);
+
+    useEffect(() => {
+        if (state.message === "success") {
+          setOpen(false);
+        }
+        console.log(state.data);
+    
+      }, [state.message]);
+
+    const onFormCreate = () => {
+        setOpen(true);
+    }
+
+    return(
+<Dialog open={open} onOpenChange={setOpen}>
+    <Button onClick={onFormCreate}>
+        <Plus className='w-4 h-4 mr-2' />
+        Create Form
+    </Button>
+  <DialogContent className='sm:max-w-[425px]'>
+    <DialogHeader>
+      <DialogTitle>Are you absolutely sure?</DialogTitle>
+    </DialogHeader>
+    <form action={formAction}>
+          <div className='grid gap-4 py-4'>
+            <Textarea id="description" name="description" required placeholder='Share what your form is about, who is it for, and what information you would like to collect. And AI will do the magic ✨' />
+          </div>
+          <DialogFooter>
+            <SubmitButton />
+            <Button variant="link">Create Manually</Button>
+          </DialogFooter>
+        </form>
+  </DialogContent>
+</Dialog>
+
+    )
+}
+
+export default FormGenerator
---
+++ src/app/page.tsx
@@ -0,0 +1,13 @@
+import { Button } from '@/components/ui/button'
+import React from 'react'
+import FormGenerator from './form-generator'
+
+const page = () => {
+  return (
+    <main className="flex min-h-screen flex-col items-center">
+      <FormGenerator/>
+  </main>
+  )
+}
+
+export default page
